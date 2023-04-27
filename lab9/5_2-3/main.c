eaf66caa3697d9f7d44483cf304c271b99087f1b
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-04-27 17:55:09 +0200

    5_2_3

---
+++ lab9/5_2-3/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int porownaj(wchar_t* napis1, wchar_t *napis2)
+{
+    int i;
+    for(i=0; napis1[i]!=0  && napis2[i] !=0; i++)
+    {
+        if(napis1[i] != napis2[i])
+        {
+            return 0;
+        }
+    }
+    if (napis1[i] !=0)
+        return 0;
+    if (napis2[i] !=0)
+        return 0;
+    return 1;
+}
+
+int main()
+{
+    wchar_t n1[] = L"ABC";
+    wchar_t n2[] = L"ABCD";
+    printf("%d\n", porownaj(n1,n2));
+    return 0;
+}
