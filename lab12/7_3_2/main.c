671da9dba8623a1dfcf8c218281f550c57d84a49
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2023-05-16 16:06:40 +0200

    7_3_2

---
+++ lab12/7_3_2/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element
+{
+    int i;
+    struct element * next;
+};
+
+struct element * wyczysc(struct element * lista)
+{
+    struct element * wsk= lista;
+    struct element * wsk2 = wsk;
+    while(wsk!= NULL)
+    {
+        wsk= wsk->next;
+        free(wsk2);
+        wsk2= wsk;
+    }
+    return NULL;
+};
+
+void wyswietl(struct element * lista)
+{
+    if (lista == NULL)
+    {
+        printf("Lista jest pusta\n");
+        return;
+    }
+    struct element * wsk= lista;
+    while(wsk !=NULL)
+    {
+        printf("%d\n", wsk->i);
+        wsk= wsk->next;
+    }
+    printf("----\n");
+}
+
+int main()
+{
+    struct element * lista = malloc(sizeof(struct element));
+    lista->i = 2;
+    lista->next = malloc(sizeof(struct element));
+    lista->next->i = -5;
+    lista->next->next= malloc(sizeof(struct element));
+    lista->next->next->i = 8;
+    lista->next->next->next= NULL;
+    wyswietl(lista);
+    lista=wyczysc(lista);
+    wyswietl(lista);
+    return 0;
+}
