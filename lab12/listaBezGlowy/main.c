03e6defe093a1aa964a7388d16c3a764ed0a42cc
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2023-05-16 15:29:56 +0200

    bez glowy

---
+++ lab12/listaBezGlowy/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element
+{
+    int i;
+    struct element * next;
+};
+
+int main()
+{
+    struct element * lista = malloc(sizeof(struct element));
+    lista->i = 2;
+    lista->next = malloc(sizeof(struct element));
+    lista->next->i = -5;
+    lista->next->next= malloc(sizeof(struct element));
+    lista->next->next->i = 8;
+    lista->next->next->next= NULL;
+    struct element * wsk= lista;
+    while(wsk !=NULL)
+    {
+        printf("%d\n", wsk->i);
+        wsk= wsk->next;
+    }
+    return 0;
+}
